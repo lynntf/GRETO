53548d88801f090236e6eea32327ae7433edec6c
Thomas Lynn <tlynn@anl.gov>
2023-12-22 10:48:32 -0600

    [m] Black formatting

--- gamma_ray_tracking/__init__.py
+++ gamma_ray_tracking/__init__.py
@@ -6,7 +6,7 @@ This software is provided without warranty and is licensed under the GNU GPL 2.0
 """
 __version__ = "1.0.0"
 
-#%% Imports
+# %% Imports
 
 from scipy.constants import physical_constants
 
@@ -19,20 +19,27 @@ from . import geometry
 from .interaction_class import Interaction
 from . import physics
 from . import cluster_tools
+
 # from . import cluster_utils
 
-#%% Define constants
-RHO_GE = 5.323 # Density of Germanium [g/cm3] (NIST)
-Z_GE = 32 # Atomic Number of Germanium [p+/atom]
-A_GE = 74 # Atomic Mass of Germanium [g/mol]
-Z_A_GE = 0.44071 # Ratio of Z/A of Germanium (NIST)
-I_GE = 350.0 # Mean excitation energy of Germanium [eV] (NIST)
-N_AV = physical_constants['Avogadro constant'][0] # Avogadro's number [atoms/mol]
-R_0 = physical_constants['classical electron radius'][0]*100 # Classical electron Radius [cm]
-MEC2 = physical_constants['electron mass energy equivalent in MeV'][0] # electron mass [MeV]
-ALPHA = physical_constants['fine-structure constant'][0] # fine structure constant 1/137
+# %% Define constants
+RHO_GE = 5.323  # Density of Germanium [g/cm3] (NIST)
+Z_GE = 32  # Atomic Number of Germanium [p+/atom]
+A_GE = 74  # Atomic Mass of Germanium [g/mol]
+Z_A_GE = 0.44071  # Ratio of Z/A of Germanium (NIST)
+I_GE = 350.0  # Mean excitation energy of Germanium [eV] (NIST)
+N_AV = physical_constants["Avogadro constant"][0]  # Avogadro's number [atoms/mol]
+R_0 = (
+    physical_constants["classical electron radius"][0] * 100
+)  # Classical electron Radius [cm]
+MEC2 = physical_constants["electron mass energy equivalent in MeV"][
+    0
+]  # electron mass [MeV]
+ALPHA = physical_constants["fine-structure constant"][
+    0
+]  # fine structure constant 1/137
 
-#%% Continue import
+# %% Continue import
 
 # def outer_test_func(event:Event, radius=None):
 #     """Test function for attribute assignment"""
@@ -44,16 +51,19 @@ ALPHA = physical_constants['fine-structure constant'][0] # fine structure consta
 
 # setattr(Event, 'outer_test_func', outer_test_func)
 
-#%% Default event and clusters for testing (non-physical)
-default_event = Event(0, [Interaction([25, 0, 0], 0.1),
-                 Interaction([25, 4, 0], 0.2),
-                 Interaction([25, 4, 3], 0.3),
-                 Interaction([0, 24, 12], 0.4),
-                 Interaction([5, 24, 0], 0.5),
-                 Interaction([0, 24, 0], 0.6),
-                 Interaction([16, 16, 16], 0.7),
-                 Interaction([0, 0, 25], 0.8),
-                 Interaction([-16, -16, -16], 0.9),
-                ]
-            )
-default_clusters = {1:[1,2,3], 2:[4,5,6], 3:[7], 4:[8,9]}
+# %% Default event and clusters for testing (non-physical)
+default_event = Event(
+    0,
+    [
+        Interaction([25, 0, 0], 0.1, ts=0, crystal_no=0, seg_no=0, energy_factor=1.0),
+        Interaction([25, 4, 0], 0.2, ts=0, crystal_no=1, seg_no=0, energy_factor=1.1),
+        Interaction([25, 4, 3], 0.3),
+        Interaction([0, 24, 12], 0.4),
+        Interaction([5, 24, 0], 0.5),
+        Interaction([0, 24, 0], 0.6),
+        Interaction([16, 16, 16], 0.7),
+        Interaction([0, 0, 25], 0.8),
+        Interaction([-16, -16, -16], 0.9),
+    ],
+)
+default_clusters = {1: [1, 2, 3], 2: [4, 5, 6], 3: [7], 4: [8, 9]}
--- gamma_ray_tracking/interaction_class.py
+++ gamma_ray_tracking/interaction_class.py
@@ -11,6 +11,7 @@ from functools import cached_property
 
 import numpy as np
 
+
 @dataclass
 class Interaction:
     """
@@ -30,7 +31,9 @@ class Interaction:
         - `2` - absorption
         - `3` - pair production
         - `99` - interaction after a pair production or possibly other interaction
+    - `energy_factor` : The correction factor used to adjust energy to match the central contact
     """
+
     x: Iterable[float]
     e: float
     ts: int = 0
@@ -40,11 +43,14 @@ class Interaction:
     event_no: int = None
     interaction_id: str = None
     interaction_type: int = None
+    energy_factor: float = 1.0
 
     def __post_init__(self):
         """Convert to numpy arrays"""
         self.x = np.array(self.x, dtype=float)
-        self.crystal_x = np.array(self.crystal_x) if self.crystal_x is not None else None
+        self.crystal_x = (
+            np.array(self.crystal_x) if self.crystal_x is not None else None
+        )
 
     @property
     def id(self):
@@ -61,15 +67,15 @@ class Interaction:
     def theta(self) -> float:
         """Angular position (spherical)"""
         if self.r == 0:
-            return 0.
-        return np.arccos(self.x[2]/self.r)
+            return 0.0
+        return np.arccos(self.x[2] / self.r)
 
     @cached_property
     def phi(self) -> float:
         """Angular position (spherical)"""
         if self.r == 0:
-            return 0.
-        return np.arctan2(self.x[1],self.x[0])
+            return 0.0
+        return np.arctan2(self.x[1], self.x[0])
 
     def __str__(self):
         out = f"Interaction(x={self.x}, e={self.e:.6f}, ts={self.ts}"
@@ -79,17 +85,19 @@ class Interaction:
             out += f", seg_no={self.seg_no}"
         if self.interaction_type is not None:
             out += f", interaction_type={self.interaction_type}"
+        if self.energy_factor > 0:
+            out += f", energy_factor={self.energy_factor}"
         out += ")"
         return out
 
-    def update_position(self, new_position:Iterable):
+    def update_position(self, new_position: Iterable):
         """
         Create a new Interaction instance with updated spatial position.
-        
+
         Args:
         - `new_position`: A new spatial position represented as an iterable of
         floats.
-        
+
         Returns:
         A new Interaction instance with the same attributes as the current
         instance except for the updated position.
@@ -104,5 +112,6 @@ class Interaction:
             event_no=self.event_no,
             interaction_id=self.interaction_id,
             interaction_type=self.interaction_type,
+            energy_factor=self.energy_factor,
         )
         return updated_instance
