3e6b2cd54be6fab651bb33e89aaeeba02d863a9c
Thomas Lynn <tlynn@anl.gov>
2024-01-18 15:42:47 -0600

    [fmt] black formatting

--- track.py
+++ track.py
@@ -13,10 +13,13 @@ def track_with_args():
     parser = argparse.ArgumentParser(description=".")
     parser.add_argument("mode2_filename", help="Path to the input mode2 file.")
     parser.add_argument("mode1_filename", help="Path to the output mode1 file.")
-    parser.add_argument("options_file", nargs="?", help="Path to the JSON/YAML options file.")
+    parser.add_argument(
+        "options_file", nargs="?", help="Path to the JSON/YAML options file."
+    )
     args = parser.parse_args()
 
     load_and_track_files(args.mode2_filename, args.mode1_filename, args.options_file)
+
 
 if __name__ == "__main__":
     track_with_args()
