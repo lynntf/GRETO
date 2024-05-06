5e28de20cfa619d9e65c899b7e668f8bd3072b06
Thomas Lynn <tlynn@anl.gov>
2024-05-06 15:52:50 -0500

    [opt] numba njit to many functions

--- greto/utils.py
+++ greto/utils.py
@@ -6,15 +6,19 @@ Utility functions
 """
 
 from copy import deepcopy
-from functools import lru_cache
-from typing import BinaryIO, Dict, Iterable, Tuple
+from functools import cache, lru_cache
+from itertools import permutations
+from typing import BinaryIO, Dict, Generator, Iterable, Tuple
 
+import numba
 import numpy as np
 from scipy.interpolate import interp1d
 
 
 @lru_cache(maxsize=100)
-def perm_to_transition(permutation: Tuple, D: int = 3) -> Tuple[np.ndarray]:
+def perm_to_transition(
+    permutation: Tuple | np.ndarray, D: int = 3
+) -> Tuple[np.ndarray]:
     """
     Transform a permutation to transition indices with dimension D
 
@@ -28,13 +32,45 @@ def perm_to_transition(permutation: Tuple, D: int = 3) -> Tuple[np.ndarray]:
     >>> A[perm_to_transition(permutation, D = 3)]  # Select i->j->k from permutation
     array([123, 234, 345, 456, 567, 678])
     """
-    p = np.array(permutation)
-    return tuple(p[i : len(p) - (D - i) + 1] for i in range(D))
+    if isinstance(permutation, tuple):
+        permutation = np.array(permutation)
+    return tuple(permutation[i : len(permutation) - (D - i) + 1] for i in range(D))
 
-def reverse_cumsum(x:np.ndarray) -> np.ndarray:
+
+def perm_to_transition_2D(permutation: Tuple | np.ndarray) -> Tuple[np.ndarray]:
+    """
+    Transform a permutation to transition indices with dimension 2
+
+    Returns 2 arrays for transitions from out[0] -> out[1]
+    """
+    if isinstance(permutation, tuple):
+        permutation = np.array(permutation)
+    return (
+        permutation[0 : len(permutation) - 1],
+        permutation[1 : len(permutation) - 0],
+    )
+
+
+def perm_to_transition_3D(permutation: Tuple | np.ndarray) -> Tuple[np.ndarray]:
+    """
+    Transform a permutation to transition indices with dimension 3
+
+    Returns 3 arrays for transitions from out[0] -> out[1] -> out[2]
+    """
+    if isinstance(permutation, tuple):
+        permutation = np.array(permutation)
+    return (
+        permutation[0 : len(permutation) - 2],
+        permutation[1 : len(permutation) - 1],
+        permutation[2 : len(permutation) - 0],
+    )
+
+
+@numba.njit
+def reverse_cumsum(x: np.ndarray) -> np.ndarray:
     """
     Reversed cumulative sum for the vector x
-    
+
     Args:
         - x: array we want the reverse cumulative sum of
 
@@ -43,6 +79,112 @@ def reverse_cumsum(x:np.ndarray) -> np.ndarray:
     """
     return np.cumsum(x[::-1])[::-1]  # Reversed cumulative sum
     # return np.flip(np.flip(x, 0).cumsum(), 0)  # slightly slower for short vec
+    # out = np.zeros((x.shape[0],))
+    # out[-1] = x[-1]
+    # for i in range(x.shape[0]-1, 0, -1):
+    #     out[i-1] = out[i] + x[i-1]
+    # return out
+
+@numba.njit
+def cumsum(x:np.ndarray) -> np.ndarray:
+    """Compiled cumulative sum"""
+    return np.cumsum(x)
+
+
+@numba.njit
+def njit_norm(a):
+    """Vector norm"""
+    b = np.sum(a**2)
+    return np.sqrt(b)
+
+
+@numba.njit
+def njit_squared_norm(a):
+    """Squared norm of vectors"""
+    return np.sum(a**2)
+
+
+@numba.njit
+def njit_sum(a):
+    """sum of a vector"""
+    return np.sum(a)
+
+
+@numba.njit
+def njit_mean(a):
+    """mean of a vector"""
+    return np.sum(a) / len(a)
+
+@numba.njit
+def njit_any(a):
+    """determine if any of the values are True"""
+    for i in numba.prange(len(a)):
+        if a[i]:
+            return True
+    return False
+
+@numba.njit
+def njit_min(a):
+    """min of a vector"""
+    return np.min(a)
+
+@numba.njit
+def njit_max(a):
+    """min of a vector"""
+    return np.max(a)
+
+
+def perm_cumulative_sums(
+    values: Iterable, r: int = None
+) -> Generator[Tuple[Tuple[int], Tuple, np.ndarray, np.ndarray], None, None]:
+    """
+    A generator for cumulative and reverse cumulative sums with caching
+
+    Because of redundancies in the values that can exist for cumulative sums of
+    permuted values, there is some slight efficiency gains that we can get by
+    simply caching some of the sum values. As an example, consider that the
+    total sum of N numbers is the same regardless of their permutation.
+    Likewise, there are only N possible values for sums of 1 value (and N-1
+    values). Because of how many permutations there are, this may not be the
+    most efficient method for larger N and the space the cache takes up can blow
+    up. Because 5! = 120 ~ 5^3 = 125, we switch to an lru_cache for larger N
+    (this can still result in savings sometimes)
+
+    Args:
+        - values: values to get the cumulative and reverse cumulative sums of
+        - r: width of the permutation (get all permutations of width r)
+
+    Returns:
+        - index_perm: permutation of indices associated with the values
+        - values_perm: the values permuted according to the index_perm
+        - cumsum: cumulative sum of the permuted values
+        - rev_cumsum: reverse cumulative sum of the permuted values
+    """
+    if len(values < 5):
+
+        @cache
+        def cache_sum(sorted_values):
+            """Compute the cumulative sum for a given set of sorted values."""
+            return sum(sorted_values)
+
+    else:
+
+        @lru_cache(maxsize=5**3)
+        def cache_sum(sorted_values):
+            """Compute the cumulative sum for a given set of sorted values."""
+            return sum(sorted_values)
+
+    for index_perm, values_perm in zip(
+        permutations(range(len(values)), r=r), permutations(values, r=r)
+    ):
+        cumsum = []  # Initialize with 0 for the empty prefix
+        rev_cumsum = []
+        for i in range(len(values_perm)):
+            prefix = cache_sum(tuple(sorted(values_perm[: i + 1])))
+            suffix = cache_sum(tuple(sorted(values_perm[i:])))
+            cumsum.append(prefix)
+            rev_cumsum.append(suffix)
+        yield index_perm, values_perm, np.array(cumsum), np.array(rev_cumsum)
 
 
 def log_interp(x: np.ndarray, xp: np.ndarray, fp: np.ndarray) -> np.ndarray:
